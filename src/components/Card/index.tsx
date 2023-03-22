083063246de1dbf5831aa25ad5e064798ecb7e66
Ryan Van Valkenburg <ryanlvv@gmail.com>
2023-03-22 13:20:29 -0700

    fix widths

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -15,7 +15,7 @@ const Card = ({ title, children, variant, href }: CardProps) => (
   >
     <section
       className={`translate container h-48 w-64 rounded-sm bg-slate-400 p-2 shadow-2xl outline outline-slate-500/50 transition duration-300 hover:translate-y-1 ${
-        !!variant ? "h-64 w-96" : "xs:h-36 xs:w-64 sm:h-32 sm:w-48"
+        !!variant ? "h-64 w-80 sm:w-96" : "xs:h-36 xs:w-64 sm:h-32 sm:w-48"
       }`}
     >
       <h1 className="font-bold tracking-tight text-white">
